dbc0eb31e7f1d5ccf399ee69de29bc6ba5a9ac14
SabinoOrbello <gemellus95@outlook.it>
2024-01-11 16:49:43 +0100

    first commit

---
+++ src/components/BookList.jsx
@@ -0,0 +1,53 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import SingleBook from "./SingleBook";
+import { Component } from "react";
+import Form from "react-bootstrap/Form";
+import CommentArea from "./CommentArea";
+class BookList extends Component {
+  state = {
+    searchQuery: "",
+    selectedBookAsin: null,
+  };
+  changeSelectedBook = (asin) => {
+    this.setState({ selectedBookAsin: asin });
+  };
+  render() {
+    return (
+      <>
+        <Row>
+          <Col>
+            <Form.Control
+              type="text"
+              placeholder="Inserisci il titolo del libro.."
+              className="mb-2"
+              value={this.state.searchQuery}
+              onChange={(e) => this.setState({ searchQuery: e.target.value })}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={8}>
+            <Row>
+              {this.props.book
+                .filter((b) => b.title.toLowerCase().includes(this.state.searchQuery))
+                .map((b) => (
+                  <Col key={b.asin}>
+                    <SingleBook
+                      book={b}
+                      selectedBookAsin={this.state.selectedBookAsin}
+                      changeSelectedBook={this.changeSelectedBook}
+                    />
+                  </Col>
+                ))}
+            </Row>
+          </Col>
+          <Col>
+            <CommentArea asin={this.state.selectedBookAsin} />
+          </Col>
+        </Row>
+      </>
+    );
+  }
+}
+export default BookList;
